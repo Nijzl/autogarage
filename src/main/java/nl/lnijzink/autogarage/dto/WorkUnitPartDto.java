2a9e3a11ac725924fd70f45b1c70aa55e446e331
NIJZL00 <lotte.nijzink@outlook.com>
2021-11-18 13:26:23 +0100

    WorkUnit WorkUnitPart

---
+++ src/main/java/nl/lnijzink/autogarage/dto/WorkUnitPartDto.java
@@ -0,0 +1,40 @@
+package nl.lnijzink.autogarage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+//@Getter
+//@Setter
+public class WorkUnitPartDto {
+    Long workUnitId;
+    Long partId;
+    Long amount;
+
+    public Long getWorkUnitId() {
+        return workUnitId;
+    }
+
+    public void setWorkUnitId(Long workUnitId) {
+        this.workUnitId = workUnitId;
+    }
+
+    public Long getPartId() {
+        return partId;
+    }
+
+    public void setPartId(Long partId) {
+        this.partId = partId;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+}
